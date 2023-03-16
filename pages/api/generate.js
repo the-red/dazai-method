bd69eaa6c8e0fdf420d9bb48d8fcbc0c6b956aed
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-03-16 16:36:23 +0900

    feat: gpt-3.5-turboモデルで太宰メソッド風の会話

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,8 +15,8 @@ export default async function (req, res) {
     return
   }
 
-  const animal = req.body.animal || ''
-  if (animal.trim().length === 0) {
+  const seken = req.body.animal || ''
+  if (seken.trim().length === 0) {
     res.status(400).json({
       error: {
         message: 'Please enter a valid animal',
@@ -26,12 +26,11 @@ export default async function (req, res) {
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: generateMessages(seken),
     })
-    res.status(200).json({ result: completion.data.choices[0].text })
+    res.status(200).json({ result: completion.data.choices[0].message.content })
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -48,14 +47,20 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal = animal[0].toUpperCase() + animal.slice(1).toLowerCase()
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`
-}
+const generateMessages = (seken) => [
+  {
+    role: 'system',
+    content: `
+      「世間」や「みんな」といった大きな主語に託した発言するに対して、
+      「世間じゃない、あなたが」「みんなじゃない、あなたが」と指摘してください。
+      返答は必ず一文で、末尾は「でしょう？」というフォーマットにしてください。
+      `,
+  },
+  // { role: 'user', content: 'それは世間が、ゆるさない' },
+  // { role: 'assistant', content: '世間じゃない。あなたが、ゆるさないのでしょう？' },
+  // { role: 'user', content: 'いまに世間から葬られる' },
+  // { role: 'assistant', content: '世間じゃない。葬むるのは、あなたでしょう？' },
+  // { role: 'user', content: 'みんなが怒っている' },
+  // { role: 'assistant', content: 'みんなじゃない、あなたが怒っているのでしょう？' },
+  { role: 'user', content: seken },
+]
