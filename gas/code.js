b0deb15aff14f34e00b903a4e10cd02e593b5662
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-03-24 13:45:28 +0900

    feat: GASのコードを追加(LINE用)

---
+++ gas/code.js
@@ -0,0 +1,93 @@
+const LINE_ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty('LINE_ACCESS_TOKEN')
+const OPENAI_APIKEY = PropertiesService.getScriptProperties().getProperty('OPENAI_APIKEY')
+
+function doPost(e) {
+  const event = JSON.parse(e.postData.contents).events[0]
+
+  const url = 'https://api.line.me/v2/bot/message/reply'
+  const replyToken = event.replyToken
+  const text = generateReplyText(event.message.text)
+
+  UrlFetchApp.fetch(url, {
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer ' + LINE_ACCESS_TOKEN,
+    },
+    method: 'post',
+    payload: JSON.stringify({
+      replyToken,
+      messages: [
+        {
+          type: 'text',
+          text,
+        },
+      ],
+    }),
+  })
+  return ContentService.createTextOutput(JSON.stringify({ content: 'post ok' })).setMimeType(
+    ContentService.MimeType.JSON
+  )
+}
+
+const generateReplyText = (userMessage) => {
+  if (userMessage === undefined) {
+    // メッセージ以外(スタンプや画像など)が送られてきた場合
+    return '太宰メソッドは使われていません。'
+  }
+
+  const requestOptions = {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + OPENAI_APIKEY,
+    },
+    payload: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: generateMessages(userMessage),
+    }),
+  }
+  const response = UrlFetchApp.fetch('https://api.openai.com/v1/chat/completions', requestOptions)
+
+  const responseText = response.getContentText()
+  const json = JSON.parse(responseText)
+  const text = json['choices'][0]['message']['content'].trim()
+  return text
+}
+
+const generateMessages = (seken) => [
+  {
+    role: 'system',
+    content: `
+あなたはChatbotとして、太宰治の小説「人間失格」の主人公のロールプレイを行います。
+以下の制約条件を厳密に守ってロールプレイを行ってください。
+
+前提知識:
+* 自らの個人的な好悪の感情などを「世間」や「みんな」といった大きな主語に託すことで、自分の責任は回避しつつ、発言に権威や説得力をもたせようとする手法を「太宰メソッド」と呼びます。
+
+主人公の行動指針:
+* Userの発言に対して「太宰メソッド」が使われているかどうかを判断して返答します。
+* 発言は常に一文だけで終わります。
+
+太宰メソッドが使われている発言の例:
+* それは世間が、ゆるさない
+* そんな事をすると、世間からひどいめに逢うぞ
+* いまに世間から葬られる
+* みんなが怒っているよ
+* 国民のあいだで議論を呼びそうだ
+* 日本人は礼儀正しい
+* 民間企業はそんなやり方しませんよ
+
+太宰メソッドが使われている発言に対する、主人公の返答の例:
+* 世間じゃない。あなたが、ゆるさないのでしょう？
+* 世間じゃない。葬むるのは、あなたでしょう？
+* みんなじゃない。あなたが、思っているのでしょう？
+* 国民じゃない、あなたが議論を呼ぼうとしているのでしょう？
+* 日本人じゃない、あなたの知っている人が礼儀正しかっただけでしょう？
+* 民間企業じゃない、あなたのいた企業の話でしょう？
+
+太宰メソッドが使われていない場合の、主人公の返答の例:
+* 太宰メソッドは使われていません。
+      `,
+  },
+  { role: 'user', content: seken },
+]
